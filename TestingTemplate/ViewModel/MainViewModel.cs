411c67742806f348fdeb496eac9269f510b30183
Дмитрий Сониев <idsonievi@gmail.com>
2019-04-02 19:20:16 +0800

    Revert "паолпаро"
    This reverts commit 89ffe572cf6e552823873070c0bbe71f671c3442.

--- TestingTemplate/ViewModel/MainViewModel.cs
+++ TestingTemplate/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -6,189 +7,189 @@ using TestingTemplate.Model;
 
 namespace TestingTemplate.ViewModel
 {
-    public class MainViewModel
-    {
-        public Action CloseAction { get; set; }
+	public class MainViewModel
+	{
+		public Action CloseAction { get; set; }
 
-        public ICommand RegistrationCommand { get; set; }
+		public ICommand RegistrationCommand { get; set; }
 
-        public ICommand AuthorizationCommand { get; set; }
+		public ICommand AuthorizationCommand { get; set; }
 
-        public ICommand SendMessageCommand { get; set; }
+		public ICommand SendMessageCommand { get; set; }
 
-        public ICommand FindUserCommand { get; set; }
+		public ICommand FindUserCommand { get; set; }
 
-        public MainViewModel()
-        {
-            LoginMainModel = new MainModel();
+		public MainViewModel()
+		{
+			LoginMainModel = new MainModel();
 
-            AuthorizationCommand = new RelayCommand(AuthCommand);
+			AuthorizationCommand = new RelayCommand(AuthCommand);
 
-            RegistrationCommand = new RelayCommand(param => Registration());
+			RegistrationCommand = new RelayCommand(param => Registration());
 
-            SendMessageCommand = new RelayCommand(param => SendCommand());
+			SendMessageCommand = new RelayCommand(param => SendCommand());
 
-            FindUserCommand = new RelayCommand(param => FindUser());
-        }
+			FindUserCommand = new RelayCommand(param => FindUser());
+		}
 
-        public MainModel LoginMainModel { get; set; }
+		public MainModel LoginMainModel { get; set; }
 
-        private void Registration()
-        {
-            Registration registrationWindow = new Registration();
+		private void Registration()
+		{
+			Registration registrationWindow = new Registration();
 
-            CloseAction();
+			CloseAction();
 
-            registrationWindow.ShowDialog();
-        }
+			registrationWindow.ShowDialog();
+		}
 
-        private void FindUser()
-        {
-            if (!string.IsNullOrEmpty(LoginMainModel.FindUser))
-            {
-                MessageBox.Show("FindUserTextboxTest");
-            }
-        }
+		private void FindUser()
+		{
+			if (string.IsNullOrEmpty(LoginMainModel.FindUser))
+			{
+				MessageBox.Show("FindUserTextboxTest");
+			}
+		}
 
-        private void SendCommand()
-        {
-            if (!string.IsNullOrEmpty(LoginMainModel.SendMessage))
-            {
-                MessageBox.Show("SendMessageTextBoxTest");
-                return;
-            }
-        }
+		private void SendCommand()
+		{
+			if (string.IsNullOrEmpty(LoginMainModel.SendMessage))
+			{
+				MessageBox.Show("SendMessageTextBoxTest");
+				return;
+			}
+		}
 
-        private void AuthCommand(object param)
-        {
-            #region Заглушка
+		private void AuthCommand(object param)
+		{
+			#region Заглушка
 
-            string login = "dmitry";
-            string passwordCheck = "dmitry";
+			string login = "dmitry";
+			string passwordCheck = "dmitry";
 
-            #endregion Заглушка
+			#endregion Заглушка
 
-            #region CheckingForNullLoginAndPassword
+			#region CheckingForNullLoginAndPassword
 
-            if (string.IsNullOrEmpty(LoginMainModel.Login))
-            {
-                MessageBox.Show("Введите логин!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+			if (string.IsNullOrEmpty(LoginMainModel.Login))
+			{
+				MessageBox.Show("Введите логин!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
 
-            var passwordBox = param as PasswordBox;
+			var passwordBox = param as PasswordBox;
 
-            if (passwordBox == null)
-                return;
+			if (passwordBox == null)
+				return;
 
-            var password = passwordBox.Password;
-            if (password == "")
-            {
-                MessageBox.Show("Введите пароль!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+			var password = passwordBox.Password;
+			if (password == "")
+			{
+				MessageBox.Show("Введите пароль!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
 
-            #endregion CheckingForNullLoginAndPassword
+			#endregion CheckingForNullLoginAndPassword
 
-            if (!string.IsNullOrEmpty(LoginMainModel.Login) && !string.IsNullOrEmpty(password))
-            {
-                if (LoginMainModel.Login == login && password == passwordCheck)
-                {
-                    MessageBox.Show("Авторизация прошла успешно.", "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
-                    return;
-                }
-                else
-                {
-                    MessageBox.Show("Логин или пароль не совпадают, пожалуйста проверьте их и повторите попытку.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return;
-                }
-            }
-        }
-    }
+			if (!string.IsNullOrEmpty(LoginMainModel.Login) && !string.IsNullOrEmpty(password))
+			{
+				if (LoginMainModel.Login == login && password == passwordCheck)
+				{
+					MessageBox.Show("Авторизация прошла успешно.", "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
+					return;
+				}
+				else
+				{
+					MessageBox.Show("Логин или пароль не совпадают, пожалуйста проверьте их и повторите попытку.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+					return;
+				}
+			}
+		}
+	}
 
-    #region TESTMVVM
+	#region TESTMVVM
 
-    //static class Test
-    //{
-    //    public static string GetHello(string a, string b) => a+b;
-    //}
+	//static class Test
+	//{
+	//    public static string GetHello(string a, string b) => a+b;
+	//}
 
-    //private string _login;
-    //public string Login
-    //{
-    //    get { return _login; }
-    //    set
-    //    {
-    //        _login = value;
-    //        OnPropertyChanged("Login");
-    //    }
-    //}
-    //public int Login { get; }=>
+	//private string _login;
+	//public string Login
+	//{
+	//    get { return _login; }
+	//    set
+	//    {
+	//        _login = value;
+	//        OnPropertyChanged("Login");
+	//    }
+	//}
+	//public int Login { get; }=>
 
-    //public class MyModel : BindableBase
-    //{
-    //    private readonly ObservableCollection<string> _myvalues = new ObservableCollection<string>();
+	//public class MyModel : BindableBase
+	//{
+	//    private readonly ObservableCollection<string> _myvalues = new ObservableCollection<string>();
 
-    //    public readonly ReadOnlyObservableCollection<string> MyPublicValues;
+	//    public readonly ReadOnlyObservableCollection<string> MyPublicValues;
 
-    //    public MyModel()
-    //    {
-    //        MyPublicValues = new ReadOnlyObservableCollection<string>(_myvalues);
-    //    }
-    //}
-    //public class MainViewModel : INotifyPropertyChanged
-    //{
-    //    public event PropertyChangedEventHandler PropertyChanged;
-    //    public void OnPropertyChanged([CallerMemberName] string prop = "")
-    //    {
-    //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-    //    }
-    //}
-    //class DelegateCommand : ICommand
-    //{
-    //    private Action<Object> execute;
-    //    private Func<object, bool> canExecute;
-    //    public event EventHandler CanExecuteChanged
-    //    {
-    //        add { CommandManager.RequerySuggested += value; }
-    //        remove { CommandManager.RequerySuggested -= value; }
-    //    }
+	//    public MyModel()
+	//    {
+	//        MyPublicValues = new ReadOnlyObservableCollection<string>(_myvalues);
+	//    }
+	//}
+	//public class MainViewModel : INotifyPropertyChanged
+	//{
+	//    public event PropertyChangedEventHandler PropertyChanged;
+	//    public void OnPropertyChanged([CallerMemberName] string prop = "")
+	//    {
+	//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+	//    }
+	//}
+	//class DelegateCommand : ICommand
+	//{
+	//    private Action<Object> execute;
+	//    private Func<object, bool> canExecute;
+	//    public event EventHandler CanExecuteChanged
+	//    {
+	//        add { CommandManager.RequerySuggested += value; }
+	//        remove { CommandManager.RequerySuggested -= value; }
+	//    }
 
-    //    public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
-    //    {
-    //        this.execute = execute;
-    //        this.canExecute = canExecute;
-    //    }
+	//    public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+	//    {
+	//        this.execute = execute;
+	//        this.canExecute = canExecute;
+	//    }
 
-    //public bool CanExecute(object parameter)
-    //{
-    //    return this.canExecute == null || this.canExecute(parameter);
-    //}
+	//public bool CanExecute(object parameter)
+	//{
+	//    return this.canExecute == null || this.canExecute(parameter);
+	//}
 
-    //public void Execute(object parameter)
-    //{
-    //    this.execute(parameter);
-    //}
-    //}
+	//public void Execute(object parameter)
+	//{
+	//    this.execute(parameter);
+	//}
+	//}
 
-    //public ICommand Click
-    //{
-    //    get { return new DelegateCommand((obj) => { }); }
-    //}
+	//public ICommand Click
+	//{
+	//    get { return new DelegateCommand((obj) => { }); }
+	//}
 
-    //public class MyViewModel
-    //{
-    //    private ICommand _authorizationCommand;
+	//public class MyViewModel
+	//{
+	//    private ICommand _authorizationCommand;
 
-    //    public ICommand AuthorizationCommand
-    //    {
-    //        get
-    //        {
-    //            return _authorizationCommand
-    //                   ?? (_authorizationCommand = new ActionCommand(() => { MessageBox.Show("test"); }));
-    //        }
-    //    }
-    //}
+	//    public ICommand AuthorizationCommand
+	//    {
+	//        get
+	//        {
+	//            return _authorizationCommand
+	//                   ?? (_authorizationCommand = new ActionCommand(() => { MessageBox.Show("test"); }));
+	//        }
+	//    }
+	//}
 
-    #endregion TESTMVVM
+	#endregion TESTMVVM
 }
